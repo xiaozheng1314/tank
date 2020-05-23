c97e8f662100c2c4014d83783f4c20eafcf8fb42
xiaozheng <1639232261>
2020-05-23 18:03:31 +0800

    坦克的启动类

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+
+public class Application {
+    public static void main(String[] args) throws InterruptedException {
+       TankFrame frame = new TankFrame();
+       while (true){
+           Thread.sleep(50);
+           frame.repaint();
+       }
+    }
+}
