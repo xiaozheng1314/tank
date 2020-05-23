26c55dbb389ac09e85cd2c87b14a9b4f8510b94b
xiaozheng <1639232261>
2020-05-23 18:06:27 +0800

    使用Java的Frame类画出显示框和坦克

---
+++ src/main/java/TankFrame.java
@@ -0,0 +1,99 @@
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+
+    Tank tank = new Tank(200, 200, Dir.DOWN);
+
+    public TankFrame() throws HeadlessException {
+        setSize(800, 600);//设置窗口大小
+        setResizable(false);//是否可以改变大小
+        setTitle("tank war");
+        setVisible(true);//显示窗口
+        this.addKeyListener(new MyKeyListener());
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        //画笔 由g对象画出坦克
+        tank.paint(g);
+    }
+
+    /**
+     * 键盘的监听器，当监听到对应的事件时做相关的移动操作
+     */
+    class MyKeyListener extends KeyAdapter {
+        boolean bL = false;
+        boolean bU = false;
+        boolean bR = false;
+        boolean bD = false;
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                default:
+                    break;
+
+            }
+            setMainTankDir();
+        }
+
+        private void setMainTankDir() {
+            if (!bL && !bR && !bD && !bU) {
+                tank.setMoving(false);
+            } else {
+                tank.setMoving(true);
+                if (bL) tank.setDir(Dir.LEFT);
+                if (bU) tank.setDir(Dir.UP);
+                if (bR) tank.setDir(Dir.RIGHT);
+                if (bD) tank.setDir(Dir.DOWN);
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                default:
+                    break;
+
+            }
+            setMainTankDir();
+        }
+    }
+}
