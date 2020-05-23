f275dcedf15605bac5789e58edcf256305df19e8
xiaozheng <1639232261>
2020-05-23 18:04:17 +0800

    定义坦克这个对象，目前具有移动的功能

---
+++ src/main/java/Tank.java
@@ -0,0 +1,43 @@
+
+import lombok.Data;
+
+import java.awt.*;
+@Data
+public class Tank {
+
+    private static final int SPEED = 10;
+
+    private int x;
+
+    private int y;
+
+    private Dir dir;
+
+    private boolean moving = false;
+
+    public Tank(int x, int y, Dir dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    public void paint(Graphics g) {
+        g.fillRect(x, y, 50, 50);
+        if(!moving) return;
+        switch (dir){
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+        }
+
+    }
+}
